6ec7fbcb4a76b62b90e68a274e88f60b2490a674
Mae-lv <70637893+Mae-lv@users.noreply.github.com>
2020-09-29 13:15:50 +0300

    Add files via upload

---
+++ task7.java
@@ -0,0 +1,29 @@
+package ru.mirea.pr5;
+
+public class task7
+{
+    private static int recursion(int n, int var)
+    {
+        if (n > 1)
+        {
+            if (n % var == 0)
+            {
+                System.out.println(var);
+                return recursion(n, var+1);
+            } else if (n % var != 0 && var < n)
+            {
+                return recursion(n, var+1);
+            } else if ( n == var)
+            {
+                System.out.println(n);
+                return 0;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args)
+    {
+        recursion(7,7);
+    }
+}
